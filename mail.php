021de372b75fbaa4e914f644d36300fa740721c3
s3784464 <s3784464@student.rmit.edu.au>
2021-05-25 23:06:25 +1000

    updated email address

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
     $formcontent="From: $name \n Message: $message";
-    $recipient = "jonomitter@outlook.com";
+    $recipient = "gdeng@pipeline.com.au";
     $subject = $_POST['subject'];
     $mailheader = "From: $email \r\n";
     mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
